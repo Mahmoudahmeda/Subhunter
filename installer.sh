600a713ae97e6157c26eafbbf63c1a7e97a16f94
Mahmoudahmeda <mahmoudahmedam205@gmail.com>
2025-10-16 14:44:15 +0300

    fix installer

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@ echo "[+] Installing the Requirments"
 pip install -r requirments.txt --break-system-package 1> /dev/null
 
 echo "[+] Adding the Command"
-echo alias subhunter="python3 Subhunter.py" >> $HOME/.bashrc
+echo alias subhunter="'python3 $(find / -name Subhunter 2>/dev/null|head -1)/Subhunter.py'" >> $HOME/.bashrc
 source $HOME/.bashrc
 
 echo "Run subhunter"
